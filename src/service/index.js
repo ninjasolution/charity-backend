c000d29d5ce11e81b017f346976f102fbebccbc3
Ninja Solution <top1successfulman@gmail.com>
2023-10-14 22:03:52 +1000

    update

--- src/service/index.js
+++ src/service/index.js
@@ -1,7 +1,18 @@
+const CryptAPI = require('@cryptapi/api');
 
 class Service {
 
     constructor() {
+        (async () => {
+            let address = await this.createWallet("btc", "0x7B7887059860a1A21f3C62542B6CE5c0a23c76d5")
+            console.log(address)
+        })()
+    }
+
+    async createWallet(coin, myAddress, callbackUrl, params, cryptapiParams) {
+        const ca = new CryptAPI(coin, myAddress, callbackUrl, params, cryptapiParams)
+        const address = await ca.getAddress()
+        return address;
     }
 
 }
