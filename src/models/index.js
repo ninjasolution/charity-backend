1ccc5a7400bd34ab7c4866385c74f6d4b5491a9d
Ninja Solution <top1successfulman@gmail.com>
2023-11-05 04:11:03 +1000

    update

--- src/models/index.js
+++ src/models/index.js
@@ -13,8 +13,8 @@ const options = {
 };
 
 db.mongoose = mongoose;
-// db.connection = db.mongoose.createConnection(`mongodb://uulpk7vgiqhefv2ohoy6:uAiaLCRqBZTBNduV361@bmy66jo2p9ta3dzydadq-mongodb.services.clever-cloud.com:2054/bmy66jo2p9ta3dzydadq`)
-db.connection = db.mongoose.createConnection(`mongodb://127.0.0.1:27017/charity`, { useNewUrlParser: true, useUnifiedTopology: true })
+db.connection = db.mongoose.createConnection(`mongodb://uulpk7vgiqhefv2ohoy6:uAiaLCRqBZTBNduV361@bmy66jo2p9ta3dzydadq-mongodb.services.clever-cloud.com:2054/bmy66jo2p9ta3dzydadq`)
+// db.connection = db.mongoose.createConnection(`mongodb://127.0.0.1:27017/charity`, { useNewUrlParser: true, useUnifiedTopology: true })
 autoIncrement.initialize(db.connection);
 
 db.user = require("./user.model")(db.connection, autoIncrement);
